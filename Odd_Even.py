20d3928c8e1ab78541cbe1de2844db316205cd96
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-20 12:41:22 +0530

    Refactor Odd_Even.py to comment out unused even/odd message logic

--- Odd_Even.py
+++ Odd_Even.py
@@ -1,4 +1,11 @@
 n = input("Enter a Number")
 n = int(n)
-message = "Number is even" if n%2 ==0 else "Number is odd"
-print(message)
+
+
+# message = "Number is even" if n%2 ==0 else "Number is odd"
+# print(message)
+
+if n> 10 and n%2 ==0:
+    print("Number is greater than 10 and even")
+else:
+    print("Number is less than or equal to 10 or odd")
