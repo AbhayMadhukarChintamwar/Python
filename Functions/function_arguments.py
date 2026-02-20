301930747f46129d8b376db0cdb704bd7d0fce3b
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-20 15:14:03 +0530

    Remove commented-out company_info function and add math_in_functions.py to calculate total expenses and their square root

--- Functions/function_arguments.py
+++ Functions/function_arguments.py
@@ -1,14 +1,21 @@
 
-def company_info(**kwargs):
+# def company_info(**kwargs):
     # if 'ticker' in kwargs:
     #     print("Ticker:", kwargs['ticker'])    
     # if 'ceo' in kwargs:
     #     print("CEO:", kwargs['ceo'])    
     # if 'revenue' in kwargs:
     #     print("Revenue:", kwargs['revenue'])
-    
-    for key in kwargs:
-        print(key, kwargs[key])
+
+#     for key in kwargs:
+#         print(key, kwargs[key])
 
 
-company_info(ticker = "AAPL", ceo = "Tim Cook", revenue = "$114.7B", market_cap = "2.5T")
+# company_info(ticker = "AAPL", ceo = "Tim Cook", revenue = "$114.7B", market_cap = "2.5T")
+# <-------------------------------------------------------------------------------------------------------------------------------------->
+
+def find_square(a):
+    return a*a
+
+x = lambda a: a*a
+print(x(5)) 
---
+++ Functions/math_in_functions.py
@@ -0,0 +1,6 @@
+import math
+expenses = [1000, 1500, 2000, 2500, 3000]
+total = math.fsum(expenses)
+print(total)
+t = math.sqrt(total)
+print(int(t))
