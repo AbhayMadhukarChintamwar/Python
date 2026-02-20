c6b60ffce56ceffc8df7825621d295751c7dc694
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-20 14:38:21 +0530

    Refactor company_info function to simplify output by removing commented code

--- Functions/function_arguments.py
+++ Functions/function_arguments.py
@@ -1,11 +1,14 @@
 
 def company_info(**kwargs):
-    if 'ticker' in kwargs:
-        print("Ticker:", kwargs['ticker'])    
-    if 'ceo' in kwargs:
-        print("CEO:", kwargs['ceo'])    
-    if 'revenue' in kwargs:
-        print("Revenue:", kwargs['revenue'])
+    # if 'ticker' in kwargs:
+    #     print("Ticker:", kwargs['ticker'])    
+    # if 'ceo' in kwargs:
+    #     print("CEO:", kwargs['ceo'])    
+    # if 'revenue' in kwargs:
+    #     print("Revenue:", kwargs['revenue'])
+    
+    for key in kwargs:
+        print(key, kwargs[key])
 
 
 company_info(ticker = "AAPL", ceo = "Tim Cook", revenue = "$114.7B", market_cap = "2.5T")
