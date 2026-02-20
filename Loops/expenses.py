0ef85968337fda4ce6d1060f9934a90d2db3d7a3
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-20 13:14:00 +0530

    Fix variable name and refactor expense calculation logic in expenses.py

--- Loops/expenses.py
+++ Loops/expenses.py
@@ -1,7 +1,10 @@
-expense = [1200, 1300, 1250, 1400, 1350]
+expenses = [1200, 1300, 1250, 1400, 1350]
 total_expense = 0
-for expense in expense:
 
-    # print(expense)
-    total_expense = total_expense + expense
-    print(total_expense)
+for i in range(len(expenses)):
+    expense = expenses[i]
+    print(f"Month {i+1}, Expense:{expense}")
+    total_expense += expense
+print(f"Total Expense: {total_expense}")
+
+   
