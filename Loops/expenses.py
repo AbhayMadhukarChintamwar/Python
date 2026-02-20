91bc228f2b481da7ad8c2e1ab9439ca31a919ff0
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-20 13:15:04 +0530

    Refactor expense iteration to use enumerate for improved readability in expenses.py

--- Loops/expenses.py
+++ Loops/expenses.py
@@ -1,7 +1,8 @@
 expenses = [1200, 1300, 1250, 1400, 1350]
 total_expense = 0
 
-for i in range(len(expenses)):
+# for i in range(len(expenses)):
+for i, expense in enumerate(expenses):
     expense = expenses[i]
     print(f"Month {i+1}, Expense:{expense}")
     total_expense += expense
