4b97688a7bc870240e071a7d9f8dc50bcb9d9726
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-20 13:44:11 +0530

    Enhance sales threshold evaluation to include month in output messages

--- Loops/break_In_loop.py
+++ Loops/break_In_loop.py
@@ -1,8 +1,10 @@
 monthly_sales = [10000, 3000, 2000,1000, 3000, 2500]
+months = ["January", "February", "March", "April", "May", "June"]
 threshold = 2000
-for sales_amount in monthly_sales:
+for sales_amount, month in zip(monthly_sales,months):
+    
     if sales_amount < threshold:
-        print(f"Sales amount {sales_amount} is below the threshold.")
+        print(f"Sales amount {sales_amount} is below the threshold in {month}.")
         break
     else:
-        print(f"Sales amount {sales_amount} is greater than or equal to the threshold.")
+        print(f"Sales amount {sales_amount} is greater than or equal to the threshold in {month}.")
