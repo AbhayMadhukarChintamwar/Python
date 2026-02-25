52a89a62e53075d87f974b33102174509fbee5e3
Abhay Chintamwar <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2026-02-25 10:12:34 +0530

    Implement operator overloading for CriketPlayers class to compare average scores, ages, and teams

--- OOPs/Class_Object/with_a_class.py
+++ OOPs/Class_Object/with_a_class.py
@@ -19,6 +19,25 @@ class CriketPlayers:
     
     def __str__(self):
         return f"{self.first_name} {self.last_name} is a player of {self.team} team and was born in {self.birth_year} and has an average score of {self.get_avg_score()}"
+    
+
+    
+    # below code is for operator overloading, where we are defining the behavior of <, > and == operators for our class objects. In this case, we are comparing the average scores of the players for < operator, ages for > operator and team names for == operator.
+
+    def __lt__(self, other):
+        self_avg = self.get_avg_score()
+        other_avg = other.get_avg_score()
+        return self_avg < other_avg
+    
+    def __gt__(self, other):
+        self_age = self.get_age()
+        other_age = other.get_age()
+        return self_age > other_age
+    
+    def __eq__(self, other):
+        self_team = self.team
+        other_team = other.team
+        return self_team == other_team
 
 virat = CriketPlayers('Virat', 'Kohli', 1988, 'India')
 virat.add_score(100)
@@ -46,6 +65,14 @@ print(f"Age of Rohit: {rohit.get_age()} years")
 
 print(virat)
 print(rohit)
+
+print(virat.get_avg_score())
+print(rohit.get_avg_score())
+
+# this print are using to understand operator overloading, where we have defined the behavior of <, > and == operators for our class objects.
+print(virat < rohit)
+print(rohit > virat)
+print(virat == rohit)
 
 
       
